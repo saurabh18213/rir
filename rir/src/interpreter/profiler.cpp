3185516e07be461d95760b7b8ea8682ab9276405
Andreas Wälchli <andreas.waelchli@me.com>
2020-06-16 13:48:12 +0000

    PMU baseline

--- rir/src/interpreter/profiler.cpp
+++ rir/src/interpreter/profiler.cpp
@@ -109,8 +109,8 @@ static void dump() {
 }
 
 void RuntimeProfiler::initProfiler() {
-    bool ENABLE_PROFILER = true; // getenv("PIR_ENABLE_PROFILER") ? true :
-                                 // false;
+    bool ENABLE_PROFILER = false; // getenv("PIR_ENABLE_PROFILER") ? true :
+                                  // false;
     if (!ENABLE_PROFILER) {
         return;
     }
