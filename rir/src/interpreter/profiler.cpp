421414c56b4b794dbfc867746dba39c9b7525c18
Andreas Wälchli <andreas.waelchli@me.com>
2020-07-04 11:32:33 +0000

    benchmark 100k t20

--- rir/src/interpreter/profiler.cpp
+++ rir/src/interpreter/profiler.cpp
@@ -141,7 +141,7 @@ void RuntimeProfiler::initProfiler() {
         PERF_COUNT_HW_INSTRUCTIONS; // Count retired hardware instructions
     pe.disabled = 1;                // Event is initially disabled
     pe.sample_type = PERF_SAMPLE_IP;
-    pe.sample_period = 1000000;
+    pe.sample_period = 100000;
     pe.exclude_kernel = 1; // excluding events that happen in the kernel-space
     pe.exclude_hv = 1;     // excluding events that happen in the hypervisor
     pe.precise_ip = 3;
