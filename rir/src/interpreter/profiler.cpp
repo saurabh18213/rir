a46fa964a9f6ed0e64579b8b9921d0a6dcc4915d
Andreas Wälchli <andreas.waelchli@me.com>
2020-07-05 20:41:26 +0000

    OVERHEAD 100k

--- rir/src/interpreter/profiler.cpp
+++ rir/src/interpreter/profiler.cpp
@@ -98,7 +98,7 @@ void RuntimeProfiler::sample(int signal) {
     // at least one slot justifies re-opt.
     if (goodValues >= (slotCount / 2) && needReopt) {
         // set global re-opt flag
-        code->flags.set(Code::Reoptimise);
+        //        code->flags.set(Code::Reoptimise);
         compilations++;
     }
 }
@@ -142,7 +142,7 @@ void RuntimeProfiler::initProfiler() {
         PERF_COUNT_HW_INSTRUCTIONS; // Count retired hardware instructions
     pe.disabled = 1;                // Event is initially disabled
     pe.sample_type = PERF_SAMPLE_IP;
-    pe.sample_period = 1000000;
+    pe.sample_period = 100000;
     pe.exclude_kernel = 1; // excluding events that happen in the kernel-space
     pe.exclude_hv = 1;     // excluding events that happen in the hypervisor
     pe.precise_ip = 3;
