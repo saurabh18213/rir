398be2486fc987413c3557c4a9399d73093afeae
Andreas Wälchli <andreas.waelchli@me.com>
2020-07-08 14:30:48 +0000

    eval2 100k T100

--- rir/src/interpreter/profiler.cpp
+++ rir/src/interpreter/profiler.cpp
@@ -80,13 +80,13 @@ void RuntimeProfiler::sample(int signal) {
                     mdEntry.needReopt = true;
                 }
             }
-            if (samples >= 50) {
+            if (samples >= 100) {
                 goodValues++;
                 if (mdEntry.needReopt) {
                     needReopt = true;
                 }
             }
-            if (samples > 500) {
+            if (samples > 1000) {
                 mdEntry.readyForReopt = false;
                 mdEntry.sampleCount = 0;
                 mdEntry.feedback.reset();
