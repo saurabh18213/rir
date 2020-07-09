665ea974b94bae6bd8c927d4d2df5d9cc2a9a224
Andreas Wälchli <andreas.waelchli@me.com>
2020-07-09 10:19:29 +0000

    bench2 500k T150

--- rir/src/interpreter/profiler.cpp
+++ rir/src/interpreter/profiler.cpp
@@ -70,7 +70,7 @@ void RuntimeProfiler::sample(int signal) {
             mdEntry.feedback.record(sxpval);
             auto samples = ++(mdEntry.sampleCount);
             slotCount++;
-            if (samples == 50) {
+            if (samples == 150) {
                 mdEntry.readyForReopt = true;
                 // check if this feedback justifies a reopt
                 pir::PirType after = pir::PirType::optimistic();
@@ -80,13 +80,13 @@ void RuntimeProfiler::sample(int signal) {
                     mdEntry.needReopt = true;
                 }
             }
-            if (samples >= 50) {
+            if (samples >= 150) {
                 goodValues++;
                 if (mdEntry.needReopt) {
                     needReopt = true;
                 }
             }
-            if (samples > 500) {
+            if (samples > 1500) {
                 mdEntry.readyForReopt = false;
                 mdEntry.sampleCount = 0;
                 mdEntry.feedback.reset();
